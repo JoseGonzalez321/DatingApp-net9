94137573cb5e7aa81577b0500beecbe15759fb66
Jose Gonzalez <jose.a.gonzalez321@gmail.com>
2025-06-27 18:28:43 -0400

    End of section 6

---
+++ client/src/app/members/member-list-detail/member-list-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-list-detail.component.html',
+  styleUrl: './member-list-detail.component.css'
+})
+export class MemberListDetailComponent {
+
+}
